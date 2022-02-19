82c8622125dafc7f88550931c7522359b996841b
d4wass <dwasilewski.93@gmail.com>
2022-02-19 13:40:32 +0100

    fix validation book form -task 2

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,22 +10,22 @@ namespace LibApp.Models
 	    [Required(ErrorMessage = "Name is required")]
 	    [StringLength(255)]
 	    public string Name { get; set; }
-	    [Required(ErrorMessage = "AuthorName is required")]
+	    [Required(ErrorMessage = "Author Name is required")]
 	    public string AuthorName { get; set; }
 	    [Required(ErrorMessage = "Genre is required")]
 	    public Genre Genre { get; set; }
 	    [Display(Name="Genre")]
 	    public byte GenreId { get; set; }
-	    [Required(ErrorMessage = "DateTime is required")]
+	    [Required(ErrorMessage = "Date Time is required")]
 	    public DateTime DateAdded { get; set; }
 	    [Display(Name="Release Date")]
-	    [Required(ErrorMessage = "ReleaseDate is required")]
+	    [Required(ErrorMessage = "Release Date is required")]
 	    public DateTime ReleaseDate { get; set; }
 	    [Display(Name = "Number in Stock")]
-	    [Required(ErrorMessage = "NumberInStock is required")]
-	    [Range(1, 20, ErrorMessage = "NumberInStock must be between 1 to 20")]
+	    [Required(ErrorMessage = "Number In Stock is required")]
+	    [Range(1, 20, ErrorMessage = "Number In Stock must be between 1 to 20")]
 	    public int NumberInStock { get; set; }
-	    [Required(ErrorMessage = "NumberAvailable is required")]
+	    [Required(ErrorMessage = "Number Available is required")]
 	    public int NumberAvailable { get; set; }
 	}
       
